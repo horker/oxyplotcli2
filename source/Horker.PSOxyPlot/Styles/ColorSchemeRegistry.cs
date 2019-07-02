312905e82cdab0e4e8a483d1cf6c4c7712c688d4
horker <horker@users.noreply.github.com>
2019-07-02 21:20:14 +0900

    Define PaletteColorScheme and color palettes based on OxyPlot preset palettes

--- source/Horker.PSOxyPlot/Styles/ColorSchemeRegistry.cs
+++ source/Horker.PSOxyPlot/Styles/ColorSchemeRegistry.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OxyPlot;
 
 namespace Horker.PSOxyPlot.Styles
 {
@@ -12,14 +13,74 @@ namespace Horker.PSOxyPlot.Styles
 
         public static IEnumerable<IColorScheme> ColorSchemes => _colorSchemes.Values;
 
+        private static void AddPaletteColorScheme(string name, IList<OxyColor> colors)
+        {
+            var p = new PaletteColorScheme(name, colors, 200);
+            Register(p);
+        }
+
         static ColorSchemeRegistry()
         {
             _colorSchemes = new Dictionary<string, IColorScheme>();
 
             _colorSchemes.Add("vanilla", new VanillaColorScheme());
             _colorSchemes.Add("ggplot", new GgplotColorScheme());
+
+            // OxyPlot's preset palettes that are hard-written in OxyPalettes.
+
+            AddPaletteColorScheme("BlueWhiteRed", new[] { OxyColors.Blue, OxyColors.White, OxyColors.Red });
+
+            AddPaletteColorScheme("Cool", new[] { OxyColors.Cyan, OxyColors.Magenta });
+
+            // AddPaletteColorScheme("Gray", new[] { OxyColors.Black, OxyColors.White });
+            AddPaletteColorScheme("Gray", OxyPalettes.Gray(200).Colors);
+
+            AddPaletteColorScheme("Hot", new[] {
+                OxyColors.Black,
+                OxyColor.FromRgb(127, 0, 0),
+                OxyColor.FromRgb(255, 127, 0),
+                OxyColor.FromRgb(255, 255, 127),
+                OxyColors.White
+            });
+
+            AddPaletteColorScheme("Hue", new[] {
+                OxyColors.Red,
+                OxyColors.Yellow,
+                OxyColors.Green,
+                OxyColors.Cyan,
+                OxyColors.Blue,
+                OxyColors.Magenta,
+            });
+
+
+            AddPaletteColorScheme("HueDistinct", new[] {
+                OxyColors.Magenta,
+                OxyColors.Blue,
+                OxyColors.Cyan,
+                OxyColors.Green,
+                OxyColors.Yellow,
+                OxyColors.Red
+            });
+
+            AddPaletteColorScheme("Jet", new[] {
+                OxyColors.DarkBlue,
+                OxyColors.Cyan,
+                OxyColors.Yellow,
+                OxyColors.Orange,
+                OxyColors.DarkRed
+            });
+
+            AddPaletteColorScheme("Rainbow", new[] {
+                OxyColors.Violet,
+                OxyColors.Indigo,
+                OxyColors.Blue,
+                OxyColors.Green,
+                OxyColors.Yellow,
+                OxyColors.Orange,
+                OxyColors.Red
+            });
         }
-        
+
         public static void Register(IColorScheme colorScheme)
         {
             _colorSchemes.Add(colorScheme.Name.ToLower(), colorScheme);
---
+++ source/Horker.PSOxyPlot/Styles/PaletteColorScheme.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OxyPlot;
+using OxyPlot.Axes;
+
+namespace Horker.PSOxyPlot.Styles
+{
+    public class PaletteColorScheme : IColorScheme
+    {
+        public string Name { get; private set; }
+        public IList<OxyColor> Colors { get; private set; }
+        public int PaletteSize { get; private set; }
+
+        public PaletteColorScheme(string name, IList<OxyColor> colors, int paletteSize)
+        {
+            Name = name;
+            Colors = colors;
+            PaletteSize = paletteSize;
+        }
+
+        public IList<OxyColor> GetDefaultColors(int count)
+        {
+            return OxyPalette.Interpolate(count, Colors.ToArray()).Colors;
+        }
+
+        public Tuple<OxyColor, OxyColor> GetCandleStickColors()
+        {
+            var colors = OxyPalette.Interpolate(2, Colors.ToArray()).Colors;
+            return Tuple.Create(colors[0], colors[1]);
+        }
+
+        public OxyPalette GetColorAxisPalette(Axis axis)
+        {
+            return OxyPalette.Interpolate(PaletteSize, Colors.ToArray());
+        }
+    }
+}
