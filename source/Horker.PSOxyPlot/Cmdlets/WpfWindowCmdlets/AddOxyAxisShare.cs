e58f62a5ee5fa24b8846b6234d49bd5f7247d5ca
horker <horker@users.noreply.github.com>
2019-07-05 20:43:29 +0900

    Add WPF window cmdlets

---
+++ source/Horker.PSOxyPlot/Cmdlets/WpfWindowCmdlets/AddOxyAxisShare.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using System.Management.Automation;
+using OxyPlot.Axes;
+
+#pragma warning disable CS1591
+
+namespace Horker.OxyPlotCli.Cmdlets
+{
+    [Cmdlet("Add", "OxyAxisShare")]
+    public class AddOxyAxisShare : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public Axis[] Axis { get; set; }
+
+        [Parameter(Position = 1, Mandatory = false)]
+        public double[] Multiplier { get; set; }
+
+        [Parameter(Position = 2, Mandatory = false)]
+        public double[] Offset { get; set; }
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+
+            if (Offset == null)
+            {
+                Offset = new double[Axis.Length];
+                for (var i = 0; i < Offset.Length; ++i)
+                    Offset[i] = 0.0;
+            }
+
+            if (Multiplier == null)
+            {
+                Multiplier = new double[Axis.Length];
+                for (var i = 0; i < Multiplier.Length; ++i)
+                    Multiplier[i] = 1.0;
+            }
+            else
+            {
+                if (Multiplier.Contains(0.0))
+                {
+                    WriteError(new ErrorRecord(new ArgumentException(), "Multiplier should not be zero", ErrorCategory.InvalidArgument, null));
+                    return;
+                }
+
+            }
+
+            if (Axis.Length != Multiplier.Length || Multiplier.Length != Offset.Length)
+            {
+                WriteError(new ErrorRecord(new ArgumentException(), "Parameter length mismatch", ErrorCategory.InvalidArgument, null));
+                return;
+            }
+
+            for (var i = 1; i < Axis.Length; ++i) {
+                Axis[i].Minimum = (Axis[0].Minimum - Offset[0]) / Multiplier[0] * Multiplier[i] + Offset[i];
+                Axis[i].Maximum = (Axis[0].Maximum - Offset[0]) / Multiplier[0] * Multiplier[i] + Offset[i];
+            }
+
+            // defensive copy
+            Axis[] sharedAxes = new Axis[Axis.Length];
+            Array.Copy(Axis, sharedAxes, Axis.Length);
+
+            bool isInternalChange = false;
+            for (var i = 0; i < Axis.Length; ++i)
+            {
+                Axis[i].AxisChanged += (sender, eventArgs) =>
+                {
+                    if (isInternalChange)
+                        return;
+
+                    var a1 = (Axis)sender;
+                    int p;
+                    for (p = 0; p < sharedAxes.Length; ++p)
+                    {
+                        if (Axis[p] == a1)
+                            break;
+                    }
+
+                    isInternalChange = true;
+
+                    for (var j = 0; j < sharedAxes.Length; ++j)
+                    {
+                        var a2 = Axis[j];
+                        if (a1 == a2)
+                            continue;
+
+                        var min = (a1.ActualMinimum - Offset[p]) / Multiplier[p] * Multiplier[j] + Offset[j];
+                        var max = (a1.ActualMaximum - Offset[p]) / Multiplier[p] * Multiplier[j] + Offset[j];
+
+                        a2.Zoom(min, max);
+                        a2.PlotModel.InvalidatePlot(false);
+                    }
+
+                    isInternalChange = false;
+                };
+            }
+
+        }
+
+    }
+}
---
+++ source/Horker.PSOxyPlot/Cmdlets/WpfWindowCmdlets/WpfWindowCmdlets.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Threading;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Reflection;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CS1591
+
+namespace Horker.OxyPlotCli.Cmdlets
+{
+    // Win32 API functions and constants to make a window invisible in the task switcher
+    // ref.
+    // https://social.msdn.microsoft.com/Forums/en-US/9c4ada92-5065-4abb-a295-d62e5ddaf2b1/wpf-window-is-showen-in-alttab-list-though-windowstylenone-showintaskbarfalse?forum=wpf
+
+    class Win32Api
+    {
+        [Flags]
+        public enum ExtendedWindowStyles
+        {
+            WS_EX_TOOLWINDOW = 0x00000080
+        }
+
+        public enum GetWindowLongFields
+        {
+            GWL_EXSTYLE = (-20)
+        }
+
+        [DllImport("kernel32.dll", EntryPoint = "SetLastError")]
+        public static extern void SetLastError(int dwErrorCode);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr", SetLastError = true)]
+        private static extern IntPtr IntSetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong", SetLastError = true)]
+        private static extern Int32 IntSetWindowLong(IntPtr hWnd, int nIndex, Int32 dwNewLong);
+
+        public static IntPtr SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong)
+        {
+            int error = 0;
+            IntPtr result = IntPtr.Zero;
+            // Win32 SetWindowLong doesn't clear error on success
+            SetLastError(0);
+
+            if (IntPtr.Size == 4)
+            {
+                // use SetWindowLong
+                Int32 tempResult = IntSetWindowLong(hWnd, nIndex, IntPtrToInt32(dwNewLong));
+                error = Marshal.GetLastWin32Error();
+                result = new IntPtr(tempResult);
+            }
+            else
+            {
+                // use SetWindowLongPtr
+                result = IntSetWindowLongPtr(hWnd, nIndex, dwNewLong);
+                error = Marshal.GetLastWin32Error();
+            }
+
+            if ((result == IntPtr.Zero) && (error != 0))
+                throw new System.ComponentModel.Win32Exception(error);
+
+            return result;
+        }
+
+        private static int IntPtrToInt32(IntPtr intPtr)
+        {
+            return unchecked((int)intPtr.ToInt64());
+        }
+
+        public static void MakeWindowInvisibleInTaskSwitcher(Window window)
+        {
+            WindowInteropHelper wndHelper = new WindowInteropHelper(window);
+
+            int exStyle = (int)GetWindowLong(wndHelper.Handle, (int)GetWindowLongFields.GWL_EXSTYLE);
+
+            exStyle |= (int)ExtendedWindowStyles.WS_EX_TOOLWINDOW;
+            SetWindowLong(wndHelper.Handle, (int)GetWindowLongFields.GWL_EXSTYLE, (IntPtr)exStyle);
+        }
+    }
+
+    public class Util
+    {
+        static private PropertyInfo IsDisposedMethod = typeof(Window).GetProperty("IsDisposed", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        static public bool IsWindowClosed(Window w)
+        {
+            return (bool)IsDisposedMethod.GetValue(w);
+        }
+
+        static public void OpenWindow(List<Window> result, AutoResetEvent e)
+        {
+            var window = new Window();
+            window.AllowsTransparency = true;
+            window.Background = Brushes.Transparent;
+            window.WindowStyle = WindowStyle.None;
+            window.ResizeMode = ResizeMode.NoResize;
+            window.ShowInTaskbar = false;
+
+            window.Loaded += (sender, args) => {
+                Win32Api.MakeWindowInvisibleInTaskSwitcher((Window)sender);
+            };
+
+            result.Add(window);
+
+            e.Set();
+
+            window.ShowDialog();
+        }
+    }
+
+    [Cmdlet("New", "WpfWindow")]
+    public class OpenWpfWindow : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = false)]
+        public string XamlString { get; set; }
+
+        [Parameter(Position = 1, Mandatory = false)]
+        public Hashtable Options { get; set; }
+
+        static private Window _rootWindow;
+        static private PowerShell _powerShell;
+
+        private void OpenRootWindow()
+        {
+            if (_rootWindow != null)
+            {
+                if (!Util.IsWindowClosed(_rootWindow))
+                    return;
+            }
+
+            var runspace = RunspaceFactory.CreateRunspace();
+            runspace.ApartmentState = ApartmentState.STA;
+            runspace.ThreadOptions = PSThreadOptions.UseNewThread;
+            runspace.Open();
+
+            _powerShell = PowerShell.Create();
+            _powerShell.Runspace = runspace;
+
+            var result = new List<Window>();
+            var e = new AutoResetEvent(false);
+
+            _powerShell.AddScript(@"
+                param($result, $event)
+                [Horker.OxyPlotCli.Cmdlets.Util]::OpenWindow($result, $event)");
+            _powerShell.AddParameter("result", result);
+            _powerShell.AddParameter("event", e);
+
+            _powerShell.BeginInvoke();
+
+            e.WaitOne();
+
+            _rootWindow = result[0];
+        }
+
+        protected override void BeginProcessing()
+        {
+            Window window = null;
+
+            OpenRootWindow();
+
+            _rootWindow.Dispatcher.Invoke(delegate {
+                if (XamlString != null)
+                    window = (Window)XamlReader.Parse(XamlString);
+                else
+                    window = new Window();
+
+                var type = window.GetType();
+                if (Options != null)
+                {
+                    foreach (DictionaryEntry entry in Options)
+                    {
+                        var prop = type.GetProperty((string)entry.Key);
+                        prop.SetValue(window, entry.Value);
+                    }
+                }
+
+                window.Show();
+            });
+
+
+            GetWpfWindowList.WindowList.Add(window);
+
+            WriteObject(window);
+        }
+    }
+
+    [Cmdlet("Close", "WpfWindow")]
+    public class CloseWpfWindow : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public Window Window { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            Window.Dispatcher.Invoke(() => {
+                Window.Close();
+            });
+        }
+    }
+
+    [Cmdlet("Invoke", "WpfWindowAction")]
+    public class InvokeWpfWindowAction : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public Window Window { get; set; }
+
+        [Parameter(Position = 1, Mandatory = true)]
+        public ScriptBlock Action { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            System.Collections.ObjectModel.Collection<PSObject> results = null;
+            Window.Dispatcher.Invoke(() => {
+                results = InvokeCommand.InvokeScript(false, Action, null);
+            });
+
+            foreach (var r in results)
+                WriteObject(r);
+        }
+    }
+
+    [Cmdlet("Test", "WpfWindowClosed")]
+    public class TestWpfWindowClosed : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public Window Window { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            WriteObject(Util.IsWindowClosed(Window));
+        }
+    }
+
+    [Cmdlet("Get", "WpfWindowList")]
+    public class GetWpfWindowList : PSCmdlet
+    {
+        static private List<Window> _windowList = new List<Window>();
+
+        static public List<Window> WindowList => _windowList;
+
+        protected override void BeginProcessing()
+        {
+            _windowList.RemoveAll((w) => { return Util.IsWindowClosed(w); });
+
+            foreach (var w in _windowList)
+                WriteObject(w);
+        }
+    }
+}
